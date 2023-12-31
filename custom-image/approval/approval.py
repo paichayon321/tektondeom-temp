b584b2952077a19fdc71ba7fdc5dc58da7150228
TOOM <35794499+paichayon321@users.noreply.github.com>
2023-12-31 10:24:54 +0700

    ffdsff

--- custom-image/approval/approval.py
+++ custom-image/approval/approval.py
@@ -16,11 +16,9 @@ def generate_random_approval_code():
 
 app = Flask(__name__)
 
-# tempcode = generate_random_approval_code()
-# print("TempCode: ", tempcode)
 
-# body = "This is approve code: " + tempcode
-# print("Body: ", body)
+#body = "This is approve code: " + tempcode
+#print("Body: ", body)
 
 # #Get the parameter value from the command line
 # sender_email = sys.argv[1]
@@ -45,9 +43,8 @@ def index():
 def process():
     approval_code = request.form['approval_code']
     action = request.form['action']
-
     tempcode = generate_random_approval_code()
-    print("TempCode: ", tempcode)
+    print("TempCode: ", tempcode)    
 
     # You can add your logic here based on the action (approve or reject)
     if action == 'approve':

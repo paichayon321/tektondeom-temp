72fd6e84c294188d9709f8524e55f52ef8a39323
TOOM <35794499+paichayon321@users.noreply.github.com>
2023-12-31 10:11:20 +0700

    dssdads

--- custom-image/approval/approval.py
+++ custom-image/approval/approval.py
@@ -21,28 +21,21 @@ print("TempCode: ", tempcode)
 
 body = "This is approve code: " + tempcode
 print("Body: ", body)
-# Get the parameter value from the command line
-sender_email = sys.argv[1]
-sender_password = sys.argv[2]
-recipient_email = sys.argv[3]
-subject = sys.argv[4]
-#body = sys.argv[5]
-#body = """
-#<html>
-#  <body>
-#    <p>This is an <b>HTML</b> email sent from Python using the Gmail SMTP server.</p>
-#  </body>
-#</html>
-#"""
-html_message = MIMEText(body, 'html')
-html_message['Subject'] = subject
-html_message['From'] = "PipelineSystem <" + sender_email + ">"
-html_message['To'] = recipient_email
-with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-   server.login(sender_email, sender_password)
-   server.sendmail(sender_email, recipient_email, html_message.as_string())
 
-print("Email sent to " + recipient_email + " successfully.")
+# #Get the parameter value from the command line
+# sender_email = sys.argv[1]
+# sender_password = sys.argv[2]
+# recipient_email = sys.argv[3]
+# subject = sys.argv[4]
+# html_message = MIMEText(body, 'html')
+# html_message['Subject'] = subject
+# html_message['From'] = "PipelineSystem <" + sender_email + ">"
+# html_message['To'] = recipient_email
+# with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+#    server.login(sender_email, sender_password)
+#    server.sendmail(sender_email, recipient_email, html_message.as_string())
+# 
+# print("Email sent to " + recipient_email + " successfully.")
 
 @app.route('/')
 def index():

27a7b552409ed5614229391321692f4f349bbec9
TOOM <35794499+paichayon321@users.noreply.github.com>
2023-12-31 10:07:16 +0700

    sdsdas

--- custom-image/approval/approval.py
+++ custom-image/approval/approval.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 import os
 import random
 import signal
@@ -10,42 +10,39 @@ if len(sys.argv) != 6:
     print("Usage: python sendgmail.py sender_mail sender_password recipient_email subject body ")
     sys.exit(1)
     
-
-app = Flask(__name__)
-
 def generate_random_approval_code():
     # Generate a random 6-digit approval code (you can adjust the length as needed)
     return ''.join([str(random.randint(0, 9)) for _ in range(12)])
 
-with app.app_context():
-    g.tempcode = generate_random_approval_code()
-    print("TempCode: ", g.tempcode)
+app = Flask(__name__)
 
-    # Get the parameter value from the command line
-    sender_email = sys.argv[1]
-    sender_password = sys.argv[2]
-    recipient_email = sys.argv[3]
-    subject = sys.argv[4]
-    #body = sys.argv[5]
-    body = g.tempcode
-    #body = """
-    #<html>
-    #  <body>
-    #    <p>This is an <b>HTML</b> email sent from Python using the Gmail SMTP server.</p>
-    #  </body>
-    #</html>
-    #"""
-    html_message = MIMEText(body, 'html')
-    html_message['Subject'] = subject
-    html_message['From'] = "PipelineSystem <" + sender_email + ">"
-    html_message['To'] = recipient_email
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
-       server.login(sender_email, sender_password)
-       server.sendmail(sender_email, recipient_email, html_message.as_string())
-    
-    print("Email sent to " + recipient_email + " successfully.")
+tempcode = generate_random_approval_code()
+print("TempCode: ", tempcode)
 
+body = "This is approve code: " + tempcode
+print("Body: ", body)
+# Get the parameter value from the command line
+sender_email = sys.argv[1]
+sender_password = sys.argv[2]
+recipient_email = sys.argv[3]
+subject = sys.argv[4]
+#body = sys.argv[5]
+#body = """
+#<html>
+#  <body>
+#    <p>This is an <b>HTML</b> email sent from Python using the Gmail SMTP server.</p>
+#  </body>
+#</html>
+#"""
+html_message = MIMEText(body, 'html')
+html_message['Subject'] = subject
+html_message['From'] = "PipelineSystem <" + sender_email + ">"
+html_message['To'] = recipient_email
+with smtplib.SMTP_SSL('smtp.gmail.com', 465) as server:
+   server.login(sender_email, sender_password)
+   server.sendmail(sender_email, recipient_email, html_message.as_string())
 
+print("Email sent to " + recipient_email + " successfully.")
 
 @app.route('/')
 def index():
@@ -55,8 +52,7 @@ def index():
 def process():
     approval_code = request.form['approval_code']
     action = request.form['action']
-    print("TempCode2: ", tempcode)
-
+    print("body: ", tempcode)
 
     # You can add your logic here based on the action (approve or reject)
     if action == 'approve':
